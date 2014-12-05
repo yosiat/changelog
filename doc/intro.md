8030e7f9e8a471504dca6ecc863539e39d29c55e
Yosi Attias <yosy101@gmail.com>
2014-12-05 18:03:30 +0200

    Lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to changelog-parser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/changelog_parser/core_test.clj
@@ -0,0 +1,7 @@
+(ns changelog-parser.core-test
+  (:require [clojure.test :refer :all]
+            [changelog-parser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
