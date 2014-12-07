7cb4ecd7af004215eb91b2053147265bd1cc7443
Yosi Attias <yosy101@gmail.com>
2014-12-07 16:37:10 +0200

    Little refactor: 1. Adding/removing comments 2. Extracting parsing logic to it's own file

---
+++ src/changelog/parser.clj
@@ -0,0 +1,59 @@
+(ns changelog.parser
+  (:require [changelog.markdown :refer :all]))
+
+
+(defn parse-changelog
+  "Parsing the given parsed markdown and returns a map where the keys are the
+  versions and the values the list of things that changes.
+
+  example:
+  For the next parsed changelog -
+  # clojure
+  ## 0.2
+  * Adding **macros**
+  * Improving performacne
+
+  ## 0.1
+  * Initial interpeter
+
+  The output will be -
+  {
+    0.2: Adding <strong>macros</strong>, Improving performance
+    0.1: Initial interpeter
+  }
+  "
+  ;; Our execution plan:
+  ;;
+  ;; We have the rest of the tags
+  ;; Our accumulated result
+  ;; and the last version headline we saw
+  ([tags result last-headline]
+  ;; If this the end of iteration
+  (if (empty? tags)
+      result
+      (let [current-tag (first tags)
+            next-tags (rest tags)
+            version (version-headline current-tag)]
+          (cond
+            ;; if current tag is a version let's continue
+            ;; IMPORTANT: this line should be before the list check
+            ;; so we will not stuck in infinite loop in-case there is no list
+            ;; after a version headline
+            (not (nil? version))
+            (parse-changelog next-tags result version)
+
+            ;; if we have headline, we are looking for list
+            (and (not (nil? last-headline)) (is-list current-tag))
+            (parse-changelog next-tags
+                       (assoc result last-headline (format-list-values current-tag))
+                       nil)
+
+            :else (parse-changelog next-tags result last-headline)))))
+
+  ([tags] (parse-changelog tags {} nil)))
+
+
+(defn parse [changelog]
+  "Parse the changelog, parse it's markdown and returns
+  a parsed changelog (see 'parse-changelog')"
+  (parse-changelog (parse-markdown changelog)))
---
+++ test/changelog/parser_test.clj
@@ -0,0 +1,52 @@
+(ns changelog.parser-test
+  (:require [clojure.test :refer :all]
+            [changelog.parser :refer :all]))
+
+(def simple-changelog (str "# This is the simplest changelog\n"
+                           "## Version 0.7\n"
+                           "* Bug 1\n"
+                           "* Bug 2\n"
+                           "* Bug 3\n"
+                           "\n"
+                           "# Version 0.2\n"
+                           "* Bug 10\n"
+                           "* Bug 9\n"
+                           "* Bug 8\n"
+                           ))
+
+
+(def changelog-headlines-without-version (str "# This is my headline\n"
+                                              "## This is another headline\n"
+                                              "* Go to eat\n"
+                                              "* Write some tests\n"
+                                              "## This my kabukation\n"))
+
+(def changelog-headlines-with-version-without-lists (str "# Eggs 0.8\n"
+                                                         "# Eggs 0.7\n"))
+
+
+
+(deftest invalid-changelog
+  (do (is (= {} (parse changelog-headlines-without-version)))
+      (is (= {} (parse changelog-headlines-with-version-without-lists)))))
+
+
+;; in this test, I test the keys and values separately and then the whole map
+;; becuase if one them fail - it will be easier to triage
+(deftest parse-versions-and-changelist
+  (do
+    ;; check the versions
+    (is (= ["0.2" "0.7"] (keys (parse simple-changelog))))
+
+    ;; check the values
+    (is (= (list (list "Bug 10" "Bug 9" "Bug 8") (list "Bug 1" "Bug 2" "Bug 3"))
+           (vals (parse simple-changelog))))
+
+    ;; check the whole shabeng
+    (is (= { "0.2" (list "Bug 10" "Bug 9" "Bug 8")
+             "0.7" (list "Bug 1" "Bug 2" "Bug 3") }
+           (parse simple-changelog)))))
+
+
+
+(run-tests)
